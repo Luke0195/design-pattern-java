6678da0e011cfdcde5b49feb15fff13fca2d0942
Lucas Santos <lucas201273@gmail.com>
2024-01-13 19:00:46 -0300

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+^
+.src/main/java/br/com/clientsprojects/Main.java,2/9/293d9ee995aa86ad2d9bf0e1b24df6c15983bb49
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+Z
+*src/main/java/br/com/application/Main.java,4/7/47e5f210b63711c88a165657c33f3a8b7396b7d6
---
+++ src/main/java/br/com/application/Main.java
@@ -0,0 +1,12 @@
+package br.com.application;
+
+// Padroẽs GOF
+
+// Padroẽs de criações - Factory Method, Abstract Factory, Singleton, Builder, Prototype.
+// Padrões estruturais - Adapter, Bridge, Composite, Decorator, Facade, FlyWeight, Proxy.
+// Padrões Comportamentais - Chain of Responsability, Command, Interpreter, Iterator, Mediator, Memeto, Observer, State, Strategy, Template, Method, Visitor
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
